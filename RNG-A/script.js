b4b8a066fa9fbd9588179ba2926b6e81dc4d93b3
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2021-08-30 09:34:40 -0400

    Add files via upload

---
+++ RNG-A/script.js
@@ -0,0 +1,14 @@
+var randomNumber = Math.floor(Math.random() * 11);
+console.log(randomNumber);
+
+var guessNumber = Math.floor(Math.random() * 11);
+console.log(guessNumber)
+
+
+function add(new_number){
+let arr = [ ];
+while (guessNumber !== randomNumber) {
+arr.push(new_number)
+}
+
+}
